49574f4d6316219931a23181f2976f1075938623
Лютиченко Павел <motry@yandex.ru>
2022-01-06 00:27:28 +0300

    Созданы 3 модели приложения reviews
    3 Основные модели и одна для реализации связи многоие-ко-многим,
    В этой таблице (GenreTitle) будут связаны произведение и жанры, к которым оно может относиться
    
    Модели:
    - Category
    - Genre
    - Title
    - GenreTitle

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,2 +1,79 @@
 from django.db import models
+from django.db.models.deletion import SET_NULL
 
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=50,
+        verbose_name='Категории контента'
+    )
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='URL категории'
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=50,
+        verbose_name='Жанры'
+    )
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='URL жанра'
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название произведения'
+    )
+    year = models.DateField(
+        auto_now_add=False,
+        verbose_name='Дата создания'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=SET_NULL,
+        related_name='reviews',
+        null=False,
+        blank=False,
+        verbose_name='Категория'
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        through='GenreTitle'
+    )
+    
+    def __str__(self) -> str:
+        return self.name
+
+# В этой модели будут связаны произведение(title_id)
+# и жанры,к которым оно может относиться(genre_id)
+class GenreTitle(models.Model):
+    title_id = models.ForeignKey(
+        Title,
+        on_delete=SET_NULL,
+        related_name='title',
+        null=False,
+        blank=False,
+        verbose_name='Произведение'        
+    )
+    genre_id = models.ForeignKey(
+        Genre,
+        on_delete=SET_NULL,
+        related_name='genre',
+        null=False,
+        blank=False,
+        verbose_name='Жанр'        
+    )
+
+    def __str__(self) -> str:
+        return f'{self.title_id} {self.genre_id}'
